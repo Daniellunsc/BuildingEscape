415f43649dcbbae064b372669a8136d7ec33c90a
Daniel Luna <daniellunsc@gmail.com>
2017-08-08 12:15:28 -0300

    Adição do ângulo de abertura da porta e do TriggerVolume selecionável na engine.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -2,10 +2,10 @@
 
 #pragma once
 
+//#include "Engine/TriggerVolume.h"
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -24,6 +24,12 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	class ATriggerVolume* PressurePlate;
 	
 };
